1fa54eda385a87fcb20c98f3e1c3b2347fb8e48d
13laze <angelx602@gmail.com>
2020-02-01 16:26:22 -0700

    Client struct created

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,10 @@
+use std::borrow::Cow;
+
 // POST request endpoint (ProcessWebServiceRequest is redundant for SOAP requests)
 // In this case since we are solely making regular POST requests it is required
 pub const ENDPOINT: &str = "/Service/PXPCommunication.asmx/ProcessWebServiceRequest";
+
+/// Struct which connects to the StudentVUE service
+pub struct Client<'a> {
+    uri: Cow<'a, str>
+}
